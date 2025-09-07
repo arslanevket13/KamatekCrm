d6423edadea28724bdac753367c941000b0a6cce
arslanevket13 <arslanevket13@gmail.com>
2025-09-07 07:09:23 +0300

    Update Customer.cs

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -1,25 +1,49 @@
-﻿using System.ComponentModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices; // Bu satırın eklendiğinden emin olun
 
 namespace KamatekCrm.Models
 {
     public class Customer : INotifyPropertyChanged
     {
         private int _id;
+        private string _name = "";
+        private string _surname = "";
+        private string _email = "";
+        private string _phone = "";
 
         public int Id
         {
             get => _id;
-            set { _id = value; OnPropertyChanged(nameof(Id)); }
+            set { _id = value; OnPropertyChanged(); }
         }
 
-        public string Name { get; set; } = "";
-        public string Surname { get; set; } = "";
-        public string Email { get; set; } = "";
-        public string Phone { get; set; } = "";
+        public string Name
+        {
+            get => _name;
+            set { _name = value; OnPropertyChanged(); }
+        }
+
+        public string Surname
+        {
+            get => _surname;
+            set { _surname = value; OnPropertyChanged(); }
+        }
+
+        public string Email
+        {
+            get => _email;
+            set { _email = value; OnPropertyChanged(); }
+        }
+
+        public string Phone
+        {
+            get => _phone;
+            set { _phone = value; OnPropertyChanged(); }
+        }
 
         public event PropertyChangedEventHandler? PropertyChanged;
 
-        protected virtual void OnPropertyChanged(string propertyName)
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
